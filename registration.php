22719b00be7a94f3bd4a34be4dc462807f7b640b
Ghanshyam Thumar <ghanshyam@commercepundit.com>
2020-04-18 18:34:43 +0530

    made theme changes

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Digimatic/ghanshyam',
+    'frontend/digimatic/ghanshyam',
     __DIR__
 );
